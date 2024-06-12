9a0129d634742bea10d394be752700e857c0dbce
Kim MyungSub <audtjqxx@naver.com>
2024-06-13 02:15:40 +0900

    [release]: modal component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import "./App.css";
+import ModalProvider from "./libs/modal/ModalProvider";
+import ModalOpenButton from "./libs/modal/ModalOpenButton";
+import ModalContainer from "./libs/modal/ModalContainer";
+import ModalCloseButton from "./libs/modal/ModalCloseButton";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ModalProvider>
+        <ModalOpenButton>열기</ModalOpenButton>
+        <ModalContainer>
+          <div>
+            <ModalCloseButton>닫기</ModalCloseButton>
+          </div>
+        </ModalContainer>
+      </ModalProvider>
+    </div>
+  );
 }
 
 export default App;
